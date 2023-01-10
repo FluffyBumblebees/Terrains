d7f823546c1974105461bc7f5890f7c244498b88
Fluffy Bumblebee <lromanic123alt@gmail.com>
2023-01-10 14:59:42 +1100

    setup some basic entrypoints

---
+++ src/main/java/net/fluffybumblebee/terrains/TerrainsClient.java
@@ -0,0 +1,10 @@
+package net.fluffybumblebee.terrains;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TerrainsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/fluffybumblebee/terrains/TerrainsCommon.java
@@ -0,0 +1,10 @@
+package net.fluffybumblebee.terrains;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TerrainsCommon implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
