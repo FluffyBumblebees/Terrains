d798ef074dddb1bfeb87c75c497f94c7f071b3fc
Fluffy Bumblebee <lromanic123alt@gmail.com>
2023-01-22 14:28:55 +1100

    pain

--- src/main/java/net/fluffybumblebee/terrains/core/datagen/TerrainsRecipes.java
+++ src/main/java/net/fluffybumblebee/terrains/core/datagen/TerrainsRecipes.java
@@ -3,26 +3,14 @@ package net.fluffybumblebee.terrains.core.datagen;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.fluffybumblebee.terrains.common.registry.sets.AllRegistrySets;
-import net.fluffybumblebee.terrains.core.TerrainsDefaults;
-import net.fluffybumblebee.terrains.util.registration.block.BlockSet;
-import net.minecraft.advancement.criterion.AbstractCriterionConditions;
-import net.minecraft.advancement.criterion.CriterionConditions;
+import net.fluffybumblebee.terrains.common.registry.sets.tree.component.WoodSet;
 import net.minecraft.data.server.recipe.RecipeJsonProvider;
-import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
-import net.minecraft.predicate.entity.EntityPredicate;
-import net.minecraft.util.Identifier;
 
 import java.util.function.Consumer;
 
+import static net.fluffybumblebee.terrains.util.datagen.DatagenUtil.*;
+
 public class TerrainsRecipes extends FabricRecipeProvider {
-    private static CriterionConditions emptyCriterion(Identifier identifier) {
-        return new AbstractCriterionConditions(identifier, EntityPredicate.Extended.EMPTY) {
-            @Override
-            public Identifier getId() {
-                return super.getId();
-            }
-        };
-    }
 
     public TerrainsRecipes(FabricDataGenerator dataGenerator) {
         super(dataGenerator);
@@ -34,6 +22,7 @@ public class TerrainsRecipes extends FabricRecipeProvider {
 
         AllRegistrySets.CRYSTAL_GEODES.getConfigQuickerator().forEach(element -> {
             twoSquared(exporter, 1, element.CORUNDUM, element.CORUNDUM_CLUSTER);
+            smelt(exporter, element.CORUNDUM, element.CRYSTAL);
             slab(exporter, element.CORUNDUM_STAIRS, element.CORUNDUM);
             stairs(exporter, element.CORUNDUM_SLAB, element.CORUNDUM);
             pane(exporter, element.CORUNDUM_PANE, element.CORUNDUM);
@@ -44,76 +33,15 @@ public class TerrainsRecipes extends FabricRecipeProvider {
     }
 
 
-    private static void stairs(
+    private static void woodSet(
             final Consumer<RecipeJsonProvider> exporter,
-            final BlockSet<?> output,
-            final BlockSet<?> input
+            final WoodSet set
     ) {
-        ShapedRecipeJsonBuilder.create(output.BLOCK, 8)
-                .criterion("", emptyCriterion(output.IDENTIFIER))
-                .group( "stairs")
-                .pattern("x  ")
-                .pattern("xx ")
-                .pattern("xxx")
-                .input('x', input.BLOCK)
-                .offerTo(exporter,
-                        TerrainsDefaults.getIdentifier(
-                                output.IDENTIFIER.getPath() + "_from_" +
-                                        input.IDENTIFIER.getPath())
-                );
-    }
-
-    private static void slab(
-            final Consumer<RecipeJsonProvider> exporter,
-            final BlockSet<?> output,
-            final BlockSet<?> input
-    ) {
-        ShapedRecipeJsonBuilder.create(output.BLOCK, 6)
-                .criterion("", emptyCriterion(output.IDENTIFIER))
-                .group("slab")
-                .pattern("xxx")
-                .input('x', input.BLOCK)
-                .offerTo(exporter,
-                        TerrainsDefaults.getIdentifier(
-                                output.IDENTIFIER.getPath() + "_from_" +
-                                        input.IDENTIFIER.getPath())
-                );
-    }
-
-    private static void pane(
-            final Consumer<RecipeJsonProvider> exporter,
-            final BlockSet<?> output,
-            final BlockSet<?> input
-    ) {
-        ShapedRecipeJsonBuilder.create(output.BLOCK, 16)
-                .criterion("", emptyCriterion(output.IDENTIFIER))
-                .group("pane")
-                .pattern("xxx")
-                .pattern("xxx")
-                .input('x', input.BLOCK)
-                .offerTo(exporter,
-                        TerrainsDefaults.getIdentifier(
-                                output.IDENTIFIER.getPath() + "_from_" +
-                                        input.IDENTIFIER.getPath())
-                );
-    }
-
-    private static void twoSquared(
-            final Consumer<RecipeJsonProvider> exporter,
-            final int outputCount,
-            final BlockSet<?> output,
-            final BlockSet<?> input
-    ) {
-        ShapedRecipeJsonBuilder.create(output.BLOCK, outputCount)
-                .criterion("", emptyCriterion(output.IDENTIFIER))
-                .group("two_squared")
-                .pattern("xx")
-                .pattern("xx")
-                .input('x', input.BLOCK)
-                .offerTo(exporter,
-                        TerrainsDefaults.getIdentifier(
-                                output.IDENTIFIER.getPath() + "_from_" +
-                                        input.IDENTIFIER.getPath())
-                );
+        stairs(exporter, set.STAIRS, set.PLANKS);
+        slab(exporter, set.SLAB, set.PLANKS);
+        boat(exporter, set.getBoat(), set.TYPE, set.PLANKS);
+        sign(exporter, set.SIGN, set.PLANKS);
+        single(exporter, set.BUTTON, 1, set.PLANKS);
+        pressurePlate(exporter, set.PRESSURE_PLATE, set.PLANKS);
     }
 }
